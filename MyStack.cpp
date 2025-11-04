369a3c0d55c144740fba54d497dddc2f463879d9
samihaati <sultanasamiha00@gmail.com>
2025-11-05 01:15:49 +0600

    Initial commit

---
+++ MyStack.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+
+class MyStack
+{
+int Stack[100], Top, MaxSize; 
+
+
+public:
+
+MyStack( int Size = 100 )
+{	MaxSize = Size; 
+    Top = 0;} 
+bool isEmpty()
+{
+	return (Top == 0);
+} 
+bool isFull()
+{
+	return (Top == MaxSize);
+}
+bool push(int Element)
+{
+	if(isFull())
+	{
+		cout<<"Stack is Full.\n";
+		return false;
+	}
+	else
+	{Stack[Top++] = Element;
+	return true;}
+}	
+bool pop()
+{
+	if(isEmpty())
+	{
+		cout<<"Stack is Empty.\n";
+		return false;
+	}
+	else
+	{
+		Top--;
+		return true;
+	}
+		
+}
+int topElement()
+{
+	return Stack[Top-1];
+}
+void show()
+{
+	if(isEmpty())
+	{
+		cout<<"Stack is Full.\n";
+		return;
+	}
+	else
+	{
+		for(int i=Top-1; i>=0;i--)
+		{
+			cout<<Stack[i]<<endl;
+		}
+	}
+} 
+};
+int main()
+{
+	MyStack s;
+	s.push(10);
+	s.show();
+	s.push(20);
+	s.show();
+	s.push(30);
+	s.show();
+	s.pop();
+	s.show();
+	s.push(40);
+}
